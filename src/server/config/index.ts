2dc24f8e8459122977fe03297de9d459e14c8535
Aaryan Shroff <unconfigured@null.spigotmc.org>
2022-05-15 15:10:27 -0400

    Fix dotenv conflict

--- src/server/config/index.ts
+++ src/server/config/index.ts
@@ -1,6 +1,6 @@
-import * as dotenv from "dotenv";
-
-dotenv.config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
 let db: Object = {
   port: Number(process.env.PORT) || 5432,
